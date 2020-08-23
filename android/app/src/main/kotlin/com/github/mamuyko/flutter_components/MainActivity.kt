be05ceda0934bb3e6ee02d89c2db1b0d9eb3eec4
k.gubaidulin <k.gubaidulin.sis@gmail.com>
2020-08-23 16:55:21 +0700

    first

---
+++ android/app/src/main/kotlin/com/github/mamuyko/flutter_components/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.github.mamuyko.flutter_components
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_components/app.dart';
+
+void main() {
+  runApp(App());
+}
+
+
+
+
